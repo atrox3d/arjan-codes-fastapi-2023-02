5051ccba83ee64fc840de4ef8d65acb5811c15f6
atrox3d <atrox3d@gmail.com>
2024-08-04 06:41:13 +0200

    tests

--- test_server.py
+++ test_server.py
@@ -1,6 +1,69 @@
+import re
 import requests
+import pytest
 
-print(requests.get('http://127.0.0.1:8000').json())
-print(requests.get('http://127.0.0.1:8000/items/0').json())
-print(requests.get('http://127.0.0.1:8000/items/55').json())
-print(requests.get('http://127.0.0.1:8000/items?name=Nails').json())
+@pytest.fixture
+def address() -> str:
+    return '127.0.0.1'
+
+@pytest.fixture
+def port() -> int:
+    return 8000
+
+@pytest.fixture
+def server(address: str, port: int) -> str:
+    return f'http://{address}:{port}'
+
+@pytest.fixture
+def hammer() -> dict:
+    return {'name': 'Hammer', 'price': 9.99, 'count': 20, 'id': 0, 'category': 'tools'}
+
+@pytest.fixture
+def pliers() -> dict:
+    return {'name': 'Pliers', 'price': 5.99, 'count': 20, 'id': 1, 'category': 'tools'}
+
+@pytest.fixture
+def nails() -> dict:
+    return {'name': 'Nails', 'price': 1.99, 'count': 20, 'id': 2, 'category': 'consumables'}
+
+@pytest.fixture
+def items(hammer: dict, pliers: dict, nails: dict) -> dict:
+    return {'items': {'0': hammer, '1': pliers, '2': nails}}
+
+@pytest.fixture
+def not_found() -> dict:
+    return {'detail': 'Not Found'}
+
+@pytest.fixture
+def query() -> dict:
+    return {'name': None, 'price': None, 'count': None, 'category': None}
+
+def test_reachable(server):
+    try:
+        response = requests.get(server)
+        assert response.status_code == 200
+    except requests.exceptions.ConnectionError:
+        pytest.exit('cannot reach server')
+
+def test_index(server, items):
+    response = requests.get(server)
+    assert response.status_code == 200
+    assert response.json() == items
+
+def test_item_0_hammer(server, hammer):
+    response = requests.get(f'{server}/items/0')
+    assert response.status_code == 200
+    assert response.json() == hammer
+
+def test_item_55_notfound(server, not_found):
+    response = requests.get(f'{server}/items/55')
+    assert response.status_code == 404
+    assert response.json() == not_found
+
+def test_query_parameters(server, query, nails):
+    response = requests.get(f'{server}/items?name=Nails')
+    assert response.status_code == 200
+    query['name'] = 'Nails'
+    assert response.json()['selection'] == [nails]
+    assert response.json() == {'query': query, 'selection': [nails]}
+
