7a50491728687530543333b0cfb6f10660939f4f
atrox3d <atrox3d@gmail.com>
2024-08-04 05:17:25 +0200

    add query parameters

--- main.py
+++ main.py
@@ -33,9 +33,38 @@ def index() -> dict[str, dict[int, Item]]:
     return {'items': items}
 
 @app.get("/items/{item_id}")
-def get_item(item_id:int) -> Item:
+def query_item_by_item_id(item_id:int) -> Item:
     if item_id not in items:
         raise HTTPException(status_code=404)
     else:
         return items[item_id]
 
+# helper type/alias: python 3.10+
+Selection = dict[
+    str, str|int|float|Category|None
+]
+
+@app.get("/items/")
+def query_items_by_parameters(
+    name: str|None=None,
+    price: float|None=None,
+    count: int|None=None,
+    category: Category|None=None
+# ) -> dict[str, Selection]:
+) -> dict[str, Selection|list[Item]]:
+# ) -> int:
+    def check_item(item:Item) -> bool:
+        return all(
+            (            
+                name is None or item.name == name,
+                price is None or item.price == price,
+                count is None or item.count == count,
+                category is None or item.category is category,
+            )        
+        )
+    
+    selection = [item for item in items.values() if check_item(item)]
+    return {
+        'query': dict(name=name, price=price, count=count, category=category),
+        'selection': selection
+    }
